829bc2bebfaa0a50a258b5c15d7ba6ee6d92ef2e
i-m-abbhay <20je0018@me.iitism.ac.in>
2023-07-20 12:47:17 +0530

    Initial Commit -> Setting up AddTask.vue

---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+   <header>
+      <h1>{{ title }}</h1>
+      <Button text="Add Task" color="green" />
+   </header>
+</template>
+
+<script>
+import Button from "./Button";
+export default {
+   name: "Header",
+   props: {
+      title: String,
+   },
+   /*
+   props: ["title"],
+   -----or------
+   props: {
+     title: String
+     -----or-----
+     title:{
+          type: String,
+          default: "Hello World"
+     }
+   }
+   */
+   components: {
+      Button,
+   },
+};
+</script>
+
+<style scoped>
+header {
+   display: flex;
+   justify-content: space-between;
+   align-items: center;
+   margin-bottom: 20px;
+}
+</style>
